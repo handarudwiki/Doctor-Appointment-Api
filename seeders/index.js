736e37cba42c14e7c57f8d1a93f69f7d560c98a1
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-05-01 16:37:36 +0700

    Resolve seeder

--- seeders/index.js
+++ seeders/index.js
@@ -3,9 +3,9 @@ const user_seeder = require("./user_seeder");
 const appointment_seeder = require("./appointment_seeder");
 
 const main = () => {
-    // user_seeder().then(() => console.info("User seeder completed")).catch((e) => console.error(e));
-    // doctor_seeder().then(() => console.info("Doctor seeder completed")).catch((e) => console.error(e))
-    appointment_seeder().then(() => console.info("Appointment seeder completed")).catch((e) => console.error(e.message))
+    user_seeder().then(() => console.info("User seeder completed")).catch((e) => console.error(e));
+    doctor_seeder().then(() => console.info("Doctor seeder completed")).catch((e) => console.error(e))
+    appointment_seeder().then(() => console.info("Appointment seeder completed")).catch((e) => console.error(e))
 }
 
 main()
