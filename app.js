c6a97486fd30c65c14c6a770dd507bb90447fcaa
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-05-23 21:13:37 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const doctorRouter = require('./routes/doctor');
-const ratingRouter = require('./routes/rating');
-const appointmentRouter = require('./routes/appointment')
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
+const doctorRouter = require("./routes/doctor")
+const ratingRouter = require("./routes/rating")
+const appointmentRouter = require("./routes/appointment")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "jade")
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/api/v1', usersRouter);
-app.use('/api/v1/doctors',doctorRouter)
-app.use('/api/v1/ratings', ratingRouter);
-app.use('/api/v1/appointment/', appointmentRouter)
+app.use("/", indexRouter)
+app.use("/api/v1", usersRouter)
+app.use("/api/v1/doctors", doctorRouter)
+app.use("/api/v1/ratings", ratingRouter)
+app.use("/api/v1/appointment/", appointmentRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render("error")
+})
 
-module.exports = app;
+module.exports = app
--- controller/UserController.js
+++ controller/UserController.js
@@ -1,212 +1,213 @@
-const bcrypt = require('bcrypt');
-const {User} = require('../models');
-const jwt = require('jsonwebtoken');
-const { where } = require('sequelize');
-const Joi = require('joi')
-const validator = require('fastest-validator');
-const user = require('../models/user');
+const bcrypt = require("bcrypt")
+const { User } = require("../models")
+const jwt = require("jsonwebtoken")
+const { where } = require("sequelize")
+const Joi = require("joi")
+const validator = require("fastest-validator")
+const user = require("../models/user")
 const v = new validator()
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
-
-
 const register = async (req, res) => {
-    try {
-        const schema = {
-            email : 'email|empty:false',
-            password : 'string|empty:false',
-            name : 'string|empty:false',
-            no_hp : 'string|empty:false',
-            gender : 'string|empty:false',
-            age : 'number|empty:false'
-        }
-
-        const validated = v.validate(req.body, schema)
-
-        if(validated.length){
-            return res.status(400).json({
-                status : 'error',
-                message : validated
-            })
-        }
-
-        const user = await User.findOne({
-            where : {email : req.body.email}
-        })
-
-        if(user){
-            return res.status(401).json({
-                status : 'error',
-                message : 'user already exist'
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-        const savedUser = await User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword,
-            no_hp : req.body.no_hp,
-            gender : req.body.gender,
-            age : req.body.age,
-        })
-
-        return res.status(200).json({
-          status : 'success',
-          data: {
-            name : savedUser.name,
-            email : savedUser.email,
-            no_hp : savedUser.no_h,
-            gender : savedUser.gender,
-            age : savedUser.age,
-            role : savedUser.role
-          }
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status : 'error',
-            message : error.message
-        })
+  try {
+    const schema = {
+      email: "email|empty:false",
+      password: "string|empty:false",
+      name: "string|empty:false",
+      no_hp: "string|empty:false",
+      gender: "string|empty:false",
+      age: "number|empty:false",
     }
+
+    const validated = v.validate(req.body, schema)
+
+    if (validated.length) {
+      return res.status(400).json({
+        status: "error",
+        message: validated,
+      })
+    }
+
+    const user = await User.findOne({
+      where: { email: req.body.email },
+    })
+
+    if (user) {
+      return res.status(401).json({
+        status: "error",
+        message: "user already exist",
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    const savedUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      no_hp: req.body.no_hp,
+      gender: req.body.gender,
+      age: req.body.age,
+    })
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        name: savedUser.name,
+        email: savedUser.email,
+        no_hp: savedUser.no_h,
+        gender: savedUser.gender,
+        age: savedUser.age,
+        role: savedUser.role,
+      },
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    })
+  }
 }
 
-const login = async(req, res) => {
-    try {
-        const schema = {
-            email : 'email|empty:false',
-            password : 'string|empty:false',
-        }
-
-        const validated = v.validate(req.body, schema)
-
-        if(validated.length){
-            return res.status(400).json({
-                status : 'error',
-                message : validated
-            })
-        }
-
-        const user = await User.findOne({
-            where : {email : req.body.email}
-        })
-
-        if(!user){
-            return res.status(401).json({
-                status : 'error',
-                message : 'username or password is incorrect'
-            })
-        }
-
-        const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
-
-        if(!isPasswordValid){
-            return res.status(401).json({
-                status : 'error',
-                message : 'username or password is incorrect'
-            })
-        }
-
-        const token = jwt.sign({id: user.id},'passwordKey')
-
-        return res.status(200).json({
-            token : token
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status : 'error',
-            message : error.message
-        })
+const login = async (req, res) => {
+  try {
+    const schema = {
+      email: "email|empty:false",
+      password: "string|empty:false",
     }
+
+    const validated = v.validate(req.body, schema)
+
+    if (validated.length) {
+      return res.status(400).json({
+        status: "error",
+        message: validated,
+      })
+    }
+
+    const user = await User.findOne({
+      where: { email: req.body.email },
+    })
+
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        message: "Email or password is incorrect",
+      })
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        status: "error",
+        message: "Email or password is incorrect",
+      })
+    }
+
+    const token = jwt.sign({ id: user.id }, "passwordKey")
+
+    return res.status(200).json({
+      token: token,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    })
+  }
 }
 
-const update = async(req, res) => {
-    try {
-        const id = req.user
-        const user = await User.findByPk(id)
+const update = async (req, res) => {
+  try {
+    const id = req.user
+    const user = await User.findByPk(id)
 
-        if(!user){
-            return res.status(404).json({
-                status : 'error',
-                message : 'User not found'
-            })
-        }
-
-        const schema = {
-            name : 'string|empty:false',
-            email : 'email|empty:false',
-            password :'string|empty:false',
-            no_hp :'string|empty:false'
-        }
-        
-        const validated = v.validate(req.body, schema)
-
-        if(validated.length){
-            return res.status(400).json({
-                status : 'error',
-                message : validated
-            })
-        }
-
-        const email = req.body.email
-        if(email){
-            const checkEmail = User.findOne({
-                where : {email : email}
-            })
-
-            if(checkEmail && email !== user.email){
-                return res.status(409).json({
-                    satus : 'error',
-                    message : 'Email already exists'
-                })
-            }
-        }
-
-        const updatedUser = await user.update({
-            email : req.body.email,
-            password : req.body.password,
-            no_hp : req.body.no_hp,
-            name : req.body.name
-        })
-
-        return res.status(200).json({
-            status : 'success',
-            data: {
-                id : updatedUser.id,
-                email : updatedUser.email,
-                no_hp : updatedUser.no_hp,
-                name : updatedUser.name
-            }
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status : 'error',
-            message : error.message
-        })
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      })
     }
+
+    const schema = {
+      name: "string|empty:false",
+      email: "email|empty:false",
+      password: "string|empty:false",
+      no_hp: "string|empty:false",
+    }
+
+    const validated = v.validate(req.body, schema)
+
+    if (validated.length) {
+      return res.status(400).json({
+        status: "error",
+        message: validated,
+      })
+    }
+
+    const email = req.body.email
+    if (email) {
+      const checkEmail = User.findOne({
+        where: { email: email },
+      })
+
+      if (checkEmail && email !== user.email) {
+        return res.status(409).json({
+          satus: "error",
+          message: "Email already exists",
+        })
+      }
+    }
+
+    const updatedUser = await user.update({
+      email: req.body.email,
+      password: req.body.password,
+      no_hp: req.body.no_hp,
+      name: req.body.name,
+    })
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        id: updatedUser.id,
+        email: updatedUser.email,
+        no_hp: updatedUser.no_hp,
+        name: updatedUser.name,
+      },
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    })
+  }
 }
 
 const detailUser = async (req, res) => {
-    try {
-        const user = await User.findByPk(req.user)
-        if (!user) {
-            return res.status(404).json({
-                status : 'error',
-                message : 'User not found'
-            })
-        }
-
-        return res.status(200).json({
-            status : 'success',
-            data : user
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status : 'error',
-            message : error.message
-        })
+  try {
+    const user = await User.findByPk(req.user)
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      })
     }
+
+    return res.status(200).json({
+      status: "success",
+      data: user,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    })
+  }
 }
 
-module.exports = {register, login, update, detailUser}
+module.exports = { register, login, update, detailUser }
