b08460cbe03876f9dd86f108d3da9da1f01f1f5c
Macbook M1 <macbookm1@Ms-MacBook-Air.local>
2024-05-27 11:21:51 +0700

    update user and change password

--- controller/UserController.js
+++ controller/UserController.js
@@ -136,7 +136,6 @@ const update = async(req, res) => {
         const schema = {
             name : 'string|empty:false',
             email : 'email|empty:false',
-            password :'string|empty:false',
             no_hp :'string|empty:false'
         }
         
@@ -209,4 +208,33 @@ const detailUser = async (req, res) => {
     }
 }
 
-module.exports = {register, login, update, detailUser}
+const updatePassword=  async (req, res)=>{
+    try {
+        const user = await User.findByPk(req.user)
+        if (!user) {
+            return res.status(404).json({
+                status : 'error',
+                message : 'User not found'
+            })
+        }
+
+        console.log(user)
+        console.log(req.body.password)
+        hashedPassword = await bcrypt.hash(req.body.password,10)
+        await user.update({
+            password : hashedPassword,
+        })
+        console.log(user)
+        return res.status(200).json({
+            status : 'success',
+            message : "updated password successfully" 
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status : "error",
+            message : error.message
+        })
+    }
+}
+
+module.exports = {register, login, update, detailUser, updatePassword}
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {register, login, update, detailUser} = require('../controller/UserController.js')
+const {register, login, update, detailUser, updatePassword} = require('../controller/UserController.js')
 const {verifyToken} = require('../middleware/verifyToken.js')
 
 /* GET home page. */
 
 router.post('/register', register)
 router.post('/login', login)
-router.put('/update-users',verifyToken, update)
+// router.put('/update-users',verifyToken, update)
 router.get('/users', verifyToken, detailUser)
+router.put("/users/change-password", verifyToken, updatePassword)
+router.put("/users", verifyToken, update)
 
 
 module.exports = router;
